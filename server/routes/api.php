7afc8d890a7470f54a99ee256a4214037051e713
rappszsz <ralphchristianandoni@gmail.com>
2025-03-28 02:31:04 +0800

    please help again

--- server/routes/api.php
+++ server/routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(GenderController::class)->group(function () {
+    Route::get('/loadGender', 'loadGenders');
     Route::post('/storeGender', 'storeGender');
 });
 
